9a01b091d3256ad4058d56d78038d7a90780d807
zhuyali <495162889@qq.com>
2016-12-30 16:17:13 +0800

    add io

--- example.js
+++ example.js
@@ -4,7 +4,13 @@ var cpu = require('./lib/cpu');
 var disk = require('./lib/disk');
 var memory = require('./lib/memory');
 var process = require('./lib/process');
+var network = require('./lib/network');
+var io = require('./lib/io');
 
 cpu.utilize_intime(3000, './cpu');
 disk.utilize(3000, './disk');
 memory.free(3000, './memory');
+network.transfer(3000, './network');
+io.busyness(3000, './io');
+process.count(3000, './process');
+
--- index.js
+++ index.js
@@ -4,3 +4,5 @@ exports.cpu = require('./lib/cpu');
 exports.process = require('./lib/process');
 exports.disk = require('./lib/disk');
 exports.memory = require('./lib/memory');
+exports.network = require('./lib/network');
+exports.io = require('./lib/busyness');
