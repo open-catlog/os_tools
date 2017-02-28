ce97b2cfc074411bff43c27cde48930b0408b585
zhuyali <495162889@qq.com>
2017-02-28 22:00:41 +0800

    add cpu mongoose

--- common/model/cpu.js
+++ common/model/cpu.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var moment = require('moment');
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -33,7 +34,44 @@ cpuSchema.statics.add = function (server, utilization) {
   });
 };
 
-cpuSchema.statics.getByServer
+cpuSchema.statics.getRecentByServer = function (server, seconds) {
+  let context = this;
+  let now = moment();
+  let secondsAgo = now.subtract(seconds, 'seconds');
+  return new Promise(function (resolve, reject) {
+    context.find({
+      server: server,
+      create_at: {
+        $gte: secondsAgo.toDate(),
+        $lt: now.toDate()
+      }
+    }, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  })
+};
+
+cpuSchema.statics.removeRecent = function (minutes) {
+  let context = this;
+  let minutesAgo = moment().subtract(minutes, 'minutes');
+  return new Promise(function (resolve, reject) {
+    context.remove(function (resolve, reject) {
+      create_at: {
+        $lt: minutesAgo.toDate()
+      }
+    }, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
 
 mongoose.model('cpu', cpuSchema);
 var cpu = mongoose.model('cpu');
