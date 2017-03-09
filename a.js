866ac0ad4acb3000031c0e1f4dbde13537fd7a0f
zhuyali <495162889@qq.com>
2017-03-09 14:28:13 +0800

    reconsitution

--- a.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var cpu = require('./gather/cpu');
-
-//cpu.add(3000, 'localhost', './test', 'add');
-cpu.get(5000, 'localhost', 6000);
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 'use strict';
 
-exports.produceCpuInfo = require('./produce/cpu');
-exports.process = require('./produce/process');
-exports.disk = require('./produce/disk');
-exports.memory = require('./produce/memory');
-exports.network = require('./produce/network');
-exports.io = require('./produce/busyness');
+var punt = require('punt');
 
-exports.gatherCpuInfo = require('./gather/cpu');
+var cpu = require('./lib/cpu');
+var process = require('./lib/process');
+var disk = require('./lib/disk');
+var memory = require('./lib/memory');
+var network = require('./lib/network');
+var io = require('./lib/io');
+
+var client = punt.bind('127.0.0.1:5000');
+var server = punt.connect('10.1.2.10:5000');
+
+cpu(5000, server);
+process(5000, server);
+disk(5000, server);
+memory(5000, server);
+network(5000, server);
+io(5000, server);
